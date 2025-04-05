1eaaebfcd7f9dc2fc866d1b19c5c8cddba6c9d5d
JAVARCE-1 <jaw.arce@gmail.com>
2025-04-04 20:21:08 -0500

    Agregar archivos de proyecto.

---
+++ RepasoW1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepasoW1
+{
+    class Class1
+    {
+        /*
+        int numero = 1;
+        string nombre = "Jav"; //referencia
+        char caracter = '1';
+        bool esMayor = true;
+
+
+        Console.WriteLine("");
+            Console.WriteLine("DIAS DE LA SEMANA");
+            string diaSemana = "lunes";
+
+            switch (diaSemana)
+            {
+                case "Lunes":
+                    Console.WriteLine("el dia es Lunes");
+                    break;
+                default:
+                    Console.WriteLine("no es dia de la semana");
+                    break;
+            }*/
+}
+}
---
+++ RepasoW1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RepasoW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            int numero;
+
+            Console.WriteLine("");
+            Console.WriteLine("=============================");
+            Console.WriteLine("Ingrese el valor a comprobar");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero es par");
+            }else
+            {
+                Console.WriteLine("El numero es impar");
+            }
+
+ 
+
+
+        }
+    }
+}

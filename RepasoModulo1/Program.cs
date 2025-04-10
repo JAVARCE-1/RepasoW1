3a0b5ac95f2ad02cfafded1c02eabf160a7edba2
JAVARCE-1 <jaw.arce@gmail.com>
2025-04-09 23:05:29 -0500

    Subiendo cambio

--- RepasoModulo1/Program.cs
+++ RepasoModulo1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace RepasoModulo1
+﻿using System.Globalization;
+
+namespace RepasoModulo1
 {
     internal class Program
     {
@@ -10,26 +12,71 @@
              * 1 2 3
              * 1 2 3 4
              */
-
+           
             int i, j, filas;
+            /*
+                       Console.WriteLine("Ingresa el numero de filas");
+                       filas = Convert.ToInt32(Console.ReadLine());
+                       Console.WriteLine("");
+
+                       for (i = 1; i <= filas; i++)
+                       {
+                           for (j = 1; j <= i; j++)
+                           {
+                               Console.Write(j + " ");
+                           }
+                           Console.Write("\n");
+                       }
+                       Console.WriteLine("\nSe acabo el bucle");
+                       */
+
+            /*
+             * -----*
+             * ----* *
+             * ---* * *
+             * --* * * *
+             * -* * * * *
+             */
 
             Console.WriteLine("Ingresa el numero de filas");
-            filas = Convert.ToInt32(Console.ReadLine());
+              filas = 5;
             Console.WriteLine("");
+            i = 1; j = 1;
+            String cad = "*";
+           // for (i = 1; i <= filas; i++)
+              for (  i = filas; i >= 1; i--)
+                {
 
-            for (i = 1; i <= filas; i++)
-            {
                 for (j = 1; j <= i; j++)
                 {
-                    Console.Write(j + " ");
+                    Console.Write( "-");
+                    //cad += "*";
                 }
+
                 Console.Write("\n");
             }
 
+            // erjecicio
+            int[] numero = new int[] { 44, 44, 32, 55 ,66, 55};
+            int indice , repetido , anterior=-1;
+            foreach (int item in numero)
+            {
+                //bool contiene = numero.Contains(3.14);
+                
+                indice = Array.IndexOf(numero, item);
+                if (indice == anterior)
+                {
+                    Console.WriteLine($"\n Valor repetido {item} en el índice: {indice}");
+                }
+                
+                anterior = indice;
+                indice = -1;
 
-            Console.WriteLine("\nSe acabo el bucle");
 
-        
-    }
+            }
+
+
+
+        }
     }
 }

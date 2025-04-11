71a2eea8263b409666d322820dc3768e3cd8f2fa
JAVARCE-1 <jaw.arce@gmail.com>
2025-04-11 14:19:40 -0500

    Subiendo cambios del Ejercicio encontrar duplicados de un Arrays

--- RepasoModulo1/Program.cs
+++ RepasoModulo1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Collections;
+using System.Globalization;
 
 namespace RepasoModulo1
 {
@@ -38,12 +39,12 @@ namespace RepasoModulo1
              * -* * * * *
              */
 
+            /*
             Console.WriteLine("Ingresa el numero de filas");
               filas = 5;
             Console.WriteLine("");
             i = 1; j = 1;
             String cad = "*";
-           // for (i = 1; i <= filas; i++)
               for (  i = filas; i >= 1; i--)
                 {
 
@@ -56,26 +57,75 @@ namespace RepasoModulo1
                 Console.Write("\n");
             }
 
-            // erjecicio
-            int[] numero = new int[] { 44, 44, 32, 55 ,66, 55};
-            int indice , repetido , anterior=-1;
-            foreach (int item in numero)
-            {
-                //bool contiene = numero.Contains(3.14);
-                
-                indice = Array.IndexOf(numero, item);
-                if (indice == anterior)
-                {
-                    Console.WriteLine($"\n Valor repetido {item} en el índice: {indice}");
-                }
-                
-                anterior = indice;
-                indice = -1;
+              */
 
+
+
+            //EJERCICIO encontrar Duplicados:
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("ENCONTRAR NUMEROS DUPLICADOS");
+            int[] numero = new int[] { 44, 44, 32, 55 ,66, 55,5,33,77,6,44};
+            int[] numOrdenados = new int[numero.Length];
+            int valorRepetido=0, indice=0;
+            ArrayList repetidos = new ArrayList();
+
+            
+            Array.Copy(numero, numOrdenados, numero.Length);
+            Array.Sort(numOrdenados);
+
+            int contarRepetido = 0;
+            for  ( i = 0; i < numOrdenados.Length; i++)
+            {
+                for (j = i; j < numOrdenados.Length; j++)
+                {
+
+                    if (numOrdenados[i] == numOrdenados[j])
+                    {
+                        indice = Array.IndexOf(numOrdenados, numOrdenados[i]);
+                        //Console.WriteLine(" " + indice);
+                        contarRepetido++;
+                        valorRepetido = numOrdenados[i];
+                    }
+                    //else
+                    //{
+                    //    contarRepetido = 1;
+                    //}
+                }
+
+
+                if (contarRepetido > 1 && numOrdenados[indice] == numOrdenados[i] ) 
+                {
+                    Console.WriteLine(numOrdenados[i] +" repetidos" + contarRepetido);
+                    repetidos.Add(numOrdenados[i]);
+                }
+                contarRepetido = 0;
 
             }
 
+            //foreach (int rep in repetidos)
+            //{
+            //    Console.WriteLine(" repetida: " +rep);
+            //}
 
+
+            //if (contarRepetido > 1)
+            //{
+            //    Console.WriteLine($"\n El número {numero[i]}  se repite : {contarRepetido} veces");
+            //}
+            //contarRepetido = 0;
+
+            /*
+            //bool contiene = numero.Contains(3.14);
+
+            indice = Array.IndexOf(numero, item);
+            if (indice == anterior)
+            {
+                Console.WriteLine($"\n Valor repetido {item} en el índice: {indice}");
+            }
+
+            anterior = indice;
+            indice = -1;
+            */
 
         }
     }
